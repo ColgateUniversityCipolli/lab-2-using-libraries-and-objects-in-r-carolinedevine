a39af8183c76907382026d29b783dec9cda39780
carolinedevine <cdevine@colgate.edu>
2025-01-28 16:20:07 -0500

    everything minus for loop

--- lab2script.R
+++ lab2script.R
@@ -27,6 +27,8 @@ album.directories <- all.directories[num.of.forwardslashes == 2 ]
 
 ### Step 3 ###
 
+#first for loop here
+
 # first album 
 test.album <- album.directories[1]
 # get all files from first album
@@ -39,6 +41,33 @@ track.files <- all.files[num.wav.files == 1]
 # empty vector 
 code.to.process <- c()
 
+# start second for loop here 
 
+# first track file 
+test.track <- track.files[1]
+track.location <- paste(test.album, "/", test.track, sep = "")
 
+track.name <- str_sub(test.track, start = 1, end = str_length(test.track)-4)
+# character matrix of file number, artist, name
+split.track.name <- str_split(track.name, "-", simplify = TRUE)
+# track name (only the name not the number etc)
+only.track.name <- split.track.name[3]
+# artist name 
+artist.name <- split.track.name[2]
 
+# album name 
+split.test.album <- str_split(test.album, "/", simplify = TRUE)
+album.name <- split.test.album[3]
+
+# [artist name]-[album name]-[track name].json
+desired.output.file <- paste(artist.name,"-",album.name,"-",only.track.name, ".json", sep = "")
+
+# command line (2.3.3.d)
+command.line <- paste("streaming_extractor_music.exe", test.track, desired.output.file)
+
+code.to.process <- c(code.to.process, command.line)
+
+#end second for loop here 
+# end first for loop here 
+
+### Step 4 ###
